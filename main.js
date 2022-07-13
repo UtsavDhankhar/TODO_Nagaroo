f7d5cffd08fe488eea794363b916f902d34ceba1
Utsav Dhankhar <utsav900@gmail.com>
2022-07-13 12:55:02 +0530

    added edit

--- main.js
+++ main.js
@@ -15,20 +15,53 @@ function display_list(t){
     li = document.getElementById("list");
     var x = document.createElement("LI");
     var y = document.createElement("button");
+    var z = document.createElement("button");
     y.innerHTML = "Delete";
+    z.innerHTML = "Edit";
+
     y.setAttribute("onclick", "delete_task(this)");
     y.setAttribute("id" , "delete");
+
+    z.setAttribute("onclick" , "edit_task(this)");
+
     var t = document.createTextNode(t);
     x.appendChild(t);
-    x.appendChild(y);
+    
     li.appendChild(x);
+    li.appendChild(y);
+    li.appendChild(z);
 }
 
 function delete_task(clicked_element){
-
-    li = clicked_element.parentNode;
+    li = clicked_element.previousSibling;
+    // li = li.previousSibling;
     li.remove();
+    li = clicked_element.nextSibling;
+    li.remove();
+    clicked_element.remove();
 }
+
+
+function edit_task(clicked_element){
+    li = clicked_element.previousSibling;
+    li = li.previousSibling;
+    text = li.innerText;
+    console.log(text);
+    task = document.getElementById("Task");
+    task.value = text;
+    
+    // li = li.parentNode;
+    // li.remove();
+    li.remove();
+    li = clicked_element.previousSibling;
+    li.remove();
+    clicked_element.remove();
+    
+
+
+
+}
+
 
 function clear_list(){
 
